eba23dcd65820f4a760ad49db12dc5d9c8013d2a
Roscoe <roscoe_roscoe@selinc.com>
2022-06-13 10:17:17 -0700

    sudoku

--- sudokuValidator.js
+++ sudokuValidator.js
@@ -23,15 +23,17 @@ function validSolution(board) {
             let rowValue = board[i][j]
             let columnValue = board[j][i];
 
-            console.log({ i }, { j })
+            // console.log({ i }, { j })
 
             if (rowRubric[rowValue] !== 0) {
+                console.log("false, duplicates in row")
                 return false;
             }
             if (rowRubric[rowValue] == 0) {
                 rowRubric[rowValue] += 1;
             }
             if (columnRubric[columnValue] !== 0) {
+                console.log("false, duplicates in column")
                 return false;
             }
             if (columnRubric[columnValue] == 0) {
@@ -41,25 +43,27 @@ function validSolution(board) {
 
             if (rowRubric[0] >= 1) {
                 total0s += rowRubric[0]
-                console.log(total0s)
+                // console.log(total0s)
             }
         }
     }
     if (total0s > 1) {
+        console.log("false, more than 1 zero")
         return false;
     }
+    console.log("true")
     return true;
 }
 
-// validSolution([[5, 3, 4, 6, 7, 8, 9, 1, 2],
-// [6, 7, 2, 1, 9, 5, 3, 4, 8],
-// [1, 9, 8, 3, 4, 2, 5, 6, 7],
-// [8, 5, 9, 7, 6, 1, 4, 2, 3],
-// [4, 2, 6, 8, 5, 3, 7, 9, 1],
-// [7, 1, 3, 9, 2, 4, 8, 5, 6],
-// [9, 6, 1, 5, 3, 7, 2, 8, 4],
-// [2, 8, 7, 4, 1, 9, 6, 3, 5],
-// [3, 4, 5, 2, 8, 6, 1, 7, 9]]);
+validSolution([[5, 3, 4, 6, 7, 8, 9, 1, 2],
+[6, 7, 2, 1, 9, 5, 3, 4, 8],
+[1, 9, 8, 3, 4, 2, 5, 6, 7],
+[8, 5, 9, 7, 6, 1, 4, 2, 3],
+[4, 2, 6, 8, 5, 3, 7, 9, 1],
+[7, 1, 3, 9, 2, 4, 8, 5, 6],
+[9, 6, 1, 5, 3, 7, 2, 8, 4],
+[2, 8, 7, 4, 1, 9, 6, 3, 5],
+[3, 4, 5, 2, 8, 6, 1, 7, 9]]);
 
 validSolution([[5, 3, 4, 6, 7, 8, 9, 1, 2],
 [6, 7, 2, 1, 9, 0, 3, 4, 8],
@@ -70,3 +74,13 @@ validSolution([[5, 3, 4, 6, 7, 8, 9, 1, 2],
 [9, 4, 1, 5, 3, 7, 2, 1, 4],
 [2, 8, 7, 4, 1, 9, 6, 3, 5],
 [3, 9, 0, 4, 8, 1, 1, 7, 9]]);
+
+validSolution([[1, 2, 3, 4, 5, 6, 7, 8, 9],
+[2, 3, 1, 5, 6, 4, 8, 9, 7],
+[3, 1, 2, 6, 4, 5, 9, 7, 8],
+[4, 5, 6, 7, 8, 9, 1, 2, 3],
+[5, 6, 4, 8, 9, 7, 2, 3, 1],
+[6, 4, 5, 9, 7, 8, 3, 1, 2],
+[7, 8, 9, 1, 2, 3, 4, 5, 6],
+[8, 9, 7, 2, 3, 1, 5, 6, 4],
+[9, 7, 8, 3, 1, 2, 6, 4, 5]])
